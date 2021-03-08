657ead02a540ff8af4dff5d2393d1d22a34329e1
Sjors <sjors.jessen@gmail.com>
2021-03-08 20:00:28 +0100

    Minor cleanup and formatting

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -28,21 +28,24 @@ const getProducts = async (): Promise<CartItemType[]> =>
     await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
-    const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+    const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
     console.log(data);
-    
+
     const getTotalItems = () => null;
     const handleAddToCart = (clickedItem: CartItemType) => null;
     const handleRemoveFromCart = () => null;
+
+    if (isLoading) 
+        return <LinearProgress/>
     
-    if(isLoading) return <LinearProgress />
-    if(error) return <div>Something went wrong...</div>
-    
+    if (error) 
+        return <div>Something went wrong...</div>
+
     return (
         <Wrapper>
             <Grid container spacing={3}>
                 {data?.map(item => (
-                    <Grid item key ={item.id} xs={12} sm={4}>
+                    <Grid item key={item.id} xs={12} sm={4}>
                         <Item item={item} handleAddToCart={handleAddToCart}/>
                     </Grid>
                 ))}
